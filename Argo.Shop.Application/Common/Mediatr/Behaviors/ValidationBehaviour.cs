45c5ac9a94fac29b82bf312349da34b79975230f
Johannes Maierhofer <jmai@ciklum.com>
2023-06-14 10:36:06 +0200

    Refactored ValidationBehaviour

--- Argo.Shop.Application/Common/Mediatr/Behaviors/ValidationBehaviour.cs
+++ Argo.Shop.Application/Common/Mediatr/Behaviors/ValidationBehaviour.cs
@@ -16,35 +16,31 @@ namespace Argo.Shop.Application.Common.Mediatr.Behaviors
 
         public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
         {
-            if (_validators.Any())
-            {
-                var context = new ValidationContext<TRequest>(request);
+            if (!_validators.Any()) 
+                return await next();
 
-                var validationResults = await Task.WhenAll(
-                    _validators.Select(v =>
-                        v.ValidateAsync(context, cancellationToken)));
+            var context = new ValidationContext<TRequest>(request);
 
-                var failures = validationResults
-                    .Where(r => r.Errors.Any())
-                    .SelectMany(r => r.Errors)
-                    .ToArray();
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v =>
+                    v.ValidateAsync(context, cancellationToken)));
 
-                if (failures.Any())
-                {
-                    // handle special case Result, Result<T>
-                    if (typeof(TResponse).IsOfTypeResult())
-                    {
-                        var failureMessages = failures.Select(f => f.ErrorMessage).ToArray();
-                        return typeof(TResponse).IsGenericResultType()
-                            ? CreateGenericErrorResult(failureMessages)
-                            : (TResponse)(object)Result.Invalid(failureMessages);
-                    }
+            var failures = validationResults
+                .Where(r => r.Errors.Any())
+                .SelectMany(r => r.Errors)
+                .ToArray();
 
-                    throw new Exceptions.ValidationException(failures);
-                }
-            }
+            if (!failures.Any()) 
+                return await next();
 
-            return await next();
+            if (!typeof(TResponse).IsOfTypeResult()) 
+                throw new Exceptions.ValidationException(failures);
+
+            // handle special case Result, Result<T>
+            var failureMessages = failures.Select(f => f.ErrorMessage).ToArray();
+            return typeof(TResponse).IsGenericResultType()
+                ? CreateGenericErrorResult(failureMessages)
+                : (TResponse)(object)Result.Invalid(failureMessages);
         }
 
         private static TResponse CreateGenericErrorResult(string[] validationFailures)
