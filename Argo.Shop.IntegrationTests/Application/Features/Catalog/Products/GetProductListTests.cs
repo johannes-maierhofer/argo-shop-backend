84625bda7e978e6345411bf3505053c682f617b0
Johannes Maierhofer <jmai@ciklum.com>
2023-06-15 11:59:40 +0200

    Added GetProductListTests

---
+++ Argo.Shop.IntegrationTests/Application/Features/Catalog/Products/GetProductListTests.cs
@@ -0,0 +1,22 @@
+﻿using Argo.Shop.Application.Common.Models;
+using Argo.Shop.Application.Features.Catalog.Products.Queries.GetProductList;
+using Xunit.Abstractions;
+
+namespace Argo.Shop.IntegrationTests.Application.Features.Catalog.Products
+{
+    public class GetProductListTests : QueryIntegrationTestBase
+    {
+        public GetProductListTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Fact]
+        public async Task GetProductList_ShouldReturnNonEmptyList_WhenOk()
+        {
+            var result = await Testing.SendAsync(new GetProductListQuery());
+            
+            Assert.Equal(ResultStatus.Ok, result.Status);
+            Assert.NotEmpty(result.Data?.Items!);
+        }
+    }
+}
